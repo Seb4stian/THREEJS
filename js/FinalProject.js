f967344df457f7f3b70644bfbc489f8a63e81b95
Eduardo Castro <ing.eduardo.castro@gmail.com>
2024-02-16 02:02:40 -0800

    Update FinalProject.js

--- js/FinalProject.js
+++ js/FinalProject.js
@@ -14,7 +14,7 @@ var clock = new THREE.Clock();
 let root = null;
 
 let controls = new function() {
-    this.NumFish = 1000;
+    this.NumFish = 100;
 	this.FishRadius = 15;
     this.Go = update2;
 }
@@ -150,37 +150,112 @@ function update2() {
 	
     scene.add(root);
 }
+
+let t = 0;
 function getDistance(child,x,y,z){
 	a = child.position.x - x;
 	b = child.position.y - y;
 	c = child.position.z - z;
 	return Math.sqrt(a*a + b*b + c*c);
 }
-function GetCloseFishes(children, x, y, z){
+function GetCloseFishes(child){
 	const nearFishes = [];
 	if (root){
 		let children = root.children;
 		for (let i = 0; i < children.length; i++) {
-			 nearFishes.push(children[i]);
+			if (getDistance(children[i], child.position.x, child.position.y, child.position.z)<controls.FishRadius && ((child.position.x != children[i].position.x)&&(child.position.x != children[i].position.x)&&(child.position.x != children[i].position.x))){
+				nearFishes.push(children[i]);
+			}
 		}
     }
 	return nearFishes;
 }
-function align(nearFishes,x,y,z){
+
+function Cohesion(nearFishes,x,y,z){
 	let n = nearFishes.length;
+	if (n == 0){
+		return [getRandomFloat(x-10,x+10),getRandomFloat(y-10,y+10),getRandomFloat(z-10,z+10)];
+	}
 	let avgX = x;
 	let avgY = y;
 	let avgZ = z;
 	for (let i = 0; i < n; i++) {
-		avgX += nearFishes[i].x;
-		avgY += nearFishes[i].y;
-		avgZ += nearFishes[i].z;
+		avgX = avgX + nearFishes[i].position.x;
+		avgY = avgY + nearFishes[i].position.y;
+		avgZ = avgZ + nearFishes[i].position.z;
+		/*if (t==0){
+		console.log("avgX = " + avgX);
+		t=t+1;
+		}*/
 	}
-	return [avgX, avgY, avgZ];
+	return [avgX/n, avgY/n, avgZ/n];
 }
-let t = 0;
-function update() {
+function separate(nearFishes,x,y,z, x0, y0, z0){
+	let n = nearFishes.length;
 	
+	if (100 < Math.abs(x)){
+		if (x > 100){
+			x = 200 - x;
+		}else{
+			x = -200 -x;
+		}
+	}
+	if (100 < Math.abs(y)){
+		if (y > 100){
+			y = 200 - y;
+		}else{
+			y = -200 -y;
+		}
+	}
+	if (100 < Math.abs(z)){
+		if (z > 100){
+			z = 200 - z;
+		}else{
+			z = -200 - z;
+		}
+	}
+	for (let i = 0; i < n; i++) {
+		let d = getDistance(nearFishes[i],x,y,z);
+		if (t==0){
+			console.log("d = " + d);
+		}
+		if (d < 2)
+			return [getRandomFloat(x-(10+controls.FishRadius),x+(10+controls.FishRadius)),getRandomFloat(y-(10+controls.FishRadius),y+(10+controls.FishRadius)),getRandomFloat(z-(10+controls.FishRadius),z+(10+controls.FishRadius))];;
+	}
+	return [x, y, z];
+}
+function nextStep(child,x,y,z){
+	
+	d = getDistance(child,x,y,z);
+	let X = x - child.position.x;
+	let Y = y - child.position.y;
+	let Z = z - child.position.z
+	return [child.position.x+X*(1/d), child.position.y+Y*(1/d), child.position.z+Z*(1/d)];
+}
+function Align(){
+}
+function update() {
+	//if ( t == 0){
+	if (root){
+		let children = root.children;
+		for (let i = 0; i < children.length; i++) {
+			let child = children[i];
+			nearFishes = GetCloseFishes(child);
+			let points = Cohesion(nearFishes,child.position.x, child.position.y, child.position.z);
+			let npoints = nextStep(child,points[0],points[1],points[2]);
+			let newPoints = separate(nearFishes,npoints[0],npoints[1],npoints[2], child.position.x, child.position.y, child.position.z);
+			if (t==0){
+				console.log("child.position.x = " + child.position.x + " newPoint[0] = "+newPoints[0]+" npoints[0]: "+npoints[0] );
+				console.log("nearFishes.length = " + nearFishes.length);
+				
+				t=t+1;
+			}
+			child.position.x = newPoints[0];
+			child.position.y = newPoints[1];
+			child.position.z = newPoints[2];
+		}
+	//}t = t+1;
+	}
 	/*let delta = clock.getDelta();
 	t += delta;
 	t %= controls.NumTorus;
@@ -201,7 +276,7 @@ function initGui() {
 
 	let gui = new dat.GUI();
     gui.add(controls, 'NumFish', 100, 2000).step(100).name('Num. Fish');
-	gui.add(controls, 'FishRadius', 5, 50).step(5).name('Fish Radius');
+	gui.add(controls, 'FishRadius', 5, 85).step(5).name('Fish Radius');
     gui.add(controls, 'Go');
 }
 
